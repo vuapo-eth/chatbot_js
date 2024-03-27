e4002bb6ad669cf19ecb1f38d4bc40176c3f4c76
Lukas Tassanyi <tassanyi@protonmail.com>
2024-03-27 09:00:12 +0200

    Create napachat2.js

---
+++ napachat2.js
@@ -0,0 +1,4 @@
+(()=>{var r={277:r=>{r.exports={Chatbot:function(){return"Hello"}}}},t={};!function o(e){var n=t[e];if(void 0!==n)return n.exports;var p=t[e]={exports:{}};return r[e](p,p.exports,o),p.exports}(277)})();
+
+
+ReactDOM.createRoot(document.getElementById('app')).render(React.createElement(Chatbot, null));
